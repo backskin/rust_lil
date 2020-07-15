abca32890b716b1451dac0d35b8046a0dfbb9c4e
backskin <backskin@outlook.com>
2020-07-15 18:07:43 +0400

    traits starts

--- src/hard_methods_module.rs
+++ src/hard_methods_module.rs
@@ -1,11 +1,17 @@
-pub fn traits(){
-    trait Animal
-    {
-        fn name(&self) -> &'static str;
-        fn talk(&self) {
-            println!("{} cannot talk", self.name());
-        }
+trait Animal
+{
+    fn create(name: &'static str) -> Self;
+    fn name(&self) -> &'static str;
+    fn talk(&self) {
+        println!("{} cannot talk", self.name());
     }
+}
+
+trait Summable<T>{
+    fn sum(&self) -> T;
+}
+
+pub fn traits(){
 
     struct Human{
         name: &'static str,
@@ -15,7 +21,21 @@ pub fn traits(){
         name: &'static str,
     }
 
+    impl Summable<i32> for Vec<i32>{
+        fn sum(&self) -> i32 {
+            let mut res = 0;
+            for x in self {
+                res += *x;
+            }
+            res
+        }
+    }
+
     impl Animal for Human {
+        fn create(name: &'static str) -> Human {
+            Human { name }
+        }
+
         fn name(&self) -> &'static str {
             self.name
         }
@@ -26,19 +46,29 @@ pub fn traits(){
     }
 
     impl Animal for Cat{
+        fn create(name: &'static str) -> Cat {
+            Cat { name }
+        }
+
         fn name(&self) -> &'static str {
             self.name
         }
 
         fn talk(&self) {
-            println!("Meow!");
+            println!("*{}, the cat*: Meow!", self.name);
         }
     }
 
-    let h = Human{ name: "John"};
+
+    let h = Human::create("John");
     h.talk();
-    let c = Cat{ name: "Misty"};
+    let c = Cat::create("Misty");
     c.talk();
+    c.talk();
+
+    let an_int_vec = vec![1,2,3,4,5,6,7,];
+    println!("vec sum = {}", an_int_vec.sum());
+
 }
 
 
